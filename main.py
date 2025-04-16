a0460b6a61c09abc7005012a145b0074f6013d01
sudonetizen <sardortolibjonov1039@gmail.com>
2025-04-16 20:01:04 +0500

    update main.py and stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_chars
 def get_book_text(file_path):
     with open(file_path) as fhandle:
         file_contents = fhandle.read()
@@ -6,6 +6,8 @@ def get_book_text(file_path):
 
 def main():
     # print(get_book_text("./books/frankenstein.txt"))
-    return f"{count_words(get_book_text("./books/frankenstein.txt"))} words found in the document"
+    words = f"{count_words(get_book_text("./books/frankenstein.txt"))} words found in the document"  
+    chars = f"{count_chars(get_book_text("./books/frankenstein.txt"))}"  
+    return words, chars 
 
 print(main())
--- stats.py
+++ stats.py
@@ -1,5 +1,13 @@
 def count_words(book_string):
     return len(book_string.split())
 
+letters_dict = {}
 def count_chars(book_string):
-    pass
+    words = book_string.split()
+    for word in words:
+        for char in word:
+            char = char.lower()
+            if char.isalpha():   
+                if char not in letters_dict: letters_dict[char] = 1
+                else: letters_dict[char] += 1
+    return letters_dict
