77ae258a59f9d367c03daef2e70f0bfba5d0672e
sudonetizen <sardortolibjonov1039@gmail.com>
2025-04-16 21:25:48 +0500

    update main.py and stats.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, list_conv
+
+path = "./books/frankenstein.txt" 
+
 def get_book_text(file_path):
     with open(file_path) as fhandle:
         file_contents = fhandle.read()
@@ -6,8 +9,14 @@ def get_book_text(file_path):
 
 def main():
     # print(get_book_text("./books/frankenstein.txt"))
-    words = f"{count_words(get_book_text("./books/frankenstein.txt"))} words found in the document"  
-    chars = f"{count_chars(get_book_text("./books/frankenstein.txt"))}"  
+    words = count_words(get_book_text(path))
+    chars = list_conv(count_chars(get_book_text(path)))
     return words, chars 
 
-print(main())
+words, chars = main()
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path[1:]}...")
+print("----------- Word Count -----------")
+print(f"Found {words} total words")
+print("--------- Character Count ---------")
+print(chars)
--- stats.py
+++ stats.py
@@ -11,3 +11,8 @@ def count_chars(book_string):
                 if char not in letters_dict: letters_dict[char] = 1
                 else: letters_dict[char] += 1
     return letters_dict
+
+def list_conv(dictionary):
+    dictionary = dict(sorted(dictionary.items(), reverse=True, key=lambda item: item[1]))
+    dict_list = "\n".join([f"{key}: {value}" for key, value in dictionary.items()])
+    return dict_list
