e19b4fcd51f56e02711b387ea9c97aa579c85865
sudonetizen <sardortolibjonov1039@gmail.com>
2025-04-16 21:56:22 +0500

    update main.py and add sys

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+import sys
 from stats import count_words, count_chars, list_conv
-
-path = "./books/frankenstein.txt" 
 
 def get_book_text(file_path):
     with open(file_path) as fhandle:
         file_contents = fhandle.read()
         return file_contents        
 
-def main():
-    # print(get_book_text("./books/frankenstein.txt"))
+def main(path):
     words = count_words(get_book_text(path))
     chars = list_conv(count_chars(get_book_text(path)))
     return words, chars 
 
-words, chars = main()
+try:
+    words, chars = main(sys.argv[1])
+except:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 print("============ BOOKBOT ============")
-print(f"Analyzing book found at {path[1:]}...")
+print(f"Analyzing book found at {sys.argv[1][1:]}...")
 print("----------- Word Count -----------")
 print(f"Found {words} total words")
 print("--------- Character Count ---------")
